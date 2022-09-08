96d3c7e4c726b1468c56eb6fecc90e0989ac6564
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-09-08 16:05:28 -0500

    Se agrega router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route , Routes} from 'react-router-dom'
-import ImageBanner from './img/in_posible_banner.png'; // Import using relative path
-import Grid from '@mui/material/Grid';
+import Inicio from './modules/Inicio'
 
 export default function App () {
   return (
-    <Grid container sx={{height:'50vh', width:'50vh'}} justifyContent="center">
-        <Grid
-            item
-            xs={12}
-            sm={12}
-            md={12}
-            sx={{
-                backgroundImage: `url(${ImageBanner})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-            }}
-        />
-    </Grid>
+    <Router>
+      <Routes>
+        <Route path={'/cartilla/:idMakerEvento'} exact element={<Inicio/>} />
+      </Routes>
+    </Router>
   );
 }
